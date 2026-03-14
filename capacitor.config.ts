5efad5411d5010dc8b917afc9e06e5105d624509
rucafonso33-debug <rucafonso33@gmail.com>
2026-03-14 21:36:24 +0000

    android build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.futureflow.app',
+  appName: 'FutureFlow',
+  webDir: 'dist'
+};
+
+export default config;
